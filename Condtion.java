d3182ff2369dbedcebbb47d9cdcfb7218e6f0c8a
pavanpyarasani <pavankumarpyarasani@gmail.com>
2024-11-04 17:59:59 +0530

    First commit
    First Date: 04-11-2024

---
+++ Condtion.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Condtion{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("insert the number:");
+        int num= sc.nextInt();
+        
+}
